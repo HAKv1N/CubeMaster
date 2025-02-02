7723e35dcd512edf2676dc837792b1991e137515
HAKv1N <egor.pestushko@yandex.ru>
2025-02-02 19:17:57 +0300

    Update 6

--- Assets/Scripts/Menu/menu_buttons.cs
+++ Assets/Scripts/Menu/menu_buttons.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class menu_buttons : MonoBehaviour
 {
     [SerializeField] private GameObject ui_interface;
     [SerializeField] private GameObject ui_settings;
+    [SerializeField] private GameObject audio_panel;
 
     private void Start() {
         ui_interface.SetActive(true);
@@ -24,5 +26,9 @@ public class menu_buttons : MonoBehaviour
     public void close_settings_menu() {
         ui_interface.SetActive(true);
         ui_settings.SetActive(false);
+    }
+
+    public void audio_settings() {
+        audio_panel.SetActive(true);
     }
 }
