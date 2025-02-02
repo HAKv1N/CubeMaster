4159b1cae6241917c053c013c33048bacc7ad787
HAKv1N <egor.pestushko@yandex.ru>
2025-02-02 18:43:19 +0300

    Update 3

--- Assets/Scripts/Game/Player.cs
+++ Assets/Scripts/Game/Player.cs
@@ -1,21 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditorInternal;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+
 public class Player : MonoBehaviour
 {
     [SerializeField] KeyCode keyone;
     [SerializeField] KeyCode keytwo;
     [SerializeField] Vector3 moveDirection;
 
-
     private void FixedUpdate()
     {
+        movement();
+    }
+
+    public void movement() {
         if (Input.GetKey(keyone))
         {
             GetComponent<Rigidbody>().linearVelocity += moveDirection;
         }
+
         if (Input.GetKey(keytwo))
         {
             GetComponent<Rigidbody>().linearVelocity -= moveDirection;
