eacc8a7d6f146df5265d0e6a247c2788cdcebcd3
HAKv1N <egor.pestushko@yandex.ru>
2025-02-05 15:55:01 +0300

    Update 12
    1. Упрощена система скинов, стала более удобнее при добавлении новых скинов

--- Assets/Scripts/Game/start_game.cs
+++ Assets/Scripts/Game/start_game.cs
@@ -5,24 +5,24 @@ public class start_game : MonoBehaviour
     [SerializeField] private int item_id;
     [SerializeField] private GameObject player_1;
     [SerializeField] private GameObject player_2;
-    [SerializeField] private Mesh skin_1_1;
-    [SerializeField] private Material skin_mat_1_1;
-    [SerializeField] private Mesh skin_1_2;
-    [SerializeField] private Material skin_mat_1_2;
+    [SerializeField] private Mesh[] skins_1 = new Mesh[1];
+    [SerializeField] private Mesh[] skins_2 = new Mesh[1];
+    [SerializeField] private Material[] materials_1 = new Material[1];
+    [SerializeField] private Material[] materials_2 = new Material[1];
 
     private void Start() {
-        item_id = shop_skins.item_1;
+        item_id = shop_skins.item_id;
         if (item_id == 1) {
-            player_1.GetComponent<MeshFilter>().mesh = skin_1_1;
-            player_1.GetComponent<MeshRenderer>().material = skin_mat_1_1;
+            player_1.GetComponent<MeshFilter>().mesh = skins_1[0];
+            player_1.GetComponent<MeshRenderer>().material = materials_1[0];
             Bounds bounds_1_1 = player_1.GetComponent<MeshFilter>().sharedMesh.bounds;
             BoxCollider player_1_collider = player_1.GetComponent<BoxCollider>();
             player_1_collider.center = bounds_1_1.center;
             player_1_collider.size = bounds_1_1.size;
             player_1.transform.localScale = new Vector3(50, 50, 50);
 
-            player_2.GetComponent<MeshFilter>().mesh = skin_1_2;
-            player_2.GetComponent<MeshRenderer>().material = skin_mat_1_2;
+            player_2.GetComponent<MeshFilter>().mesh = skins_2[0];
+            player_2.GetComponent<MeshRenderer>().material = materials_2[0];
             Bounds bounds_1_2 = player_2.GetComponent<MeshFilter>().sharedMesh.bounds;
             BoxCollider player_2_collider = player_2.GetComponent<BoxCollider>();
             player_2_collider.center = bounds_1_2.center;
--- Assets/Scripts/Menu/shop_skins.cs
+++ Assets/Scripts/Menu/shop_skins.cs
@@ -3,13 +3,13 @@ using UnityEngine;
 public class shop_skins : MonoBehaviour
 {
     [SerializeField] private GameObject skins_panel;
-    [SerializeField] public static int item_1 = 1;
+    [SerializeField] public static int item_id = 0;
 
     public void open_skins_buy() {
         skins_panel.SetActive(true);
     }
 
     public void buy_skin() {
-        item_1 = 1;
+        item_id = 1;
     }
 }
