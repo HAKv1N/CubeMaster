44f59e836fb000f95a15b3b156038f0d374ecbd1
HAKv1N <egor.pestushko@yandex.ru>
2025-02-08 22:24:41 +0300

    Update 15
    1. Изменена система покупки скинов
    2. Добавлен выбор скина в магазине(если он куплен)

--- Assets/Scripts/Menu/shop_skins.cs
+++ Assets/Scripts/Menu/shop_skins.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class shop_skins : MonoBehaviour
@@ -5,6 +6,7 @@ public class shop_skins : MonoBehaviour
     [SerializeField] private GameObject skins_panel;
     [SerializeField] public static int skin_id = 0;
     [SerializeField] public static float money = 100f;
+    [SerializeField] public static bool skin_1_buy = false;
 
     public void open_skins_buy() {
         skins_panel.SetActive(true);
@@ -12,8 +14,14 @@ public class shop_skins : MonoBehaviour
 
     public void buy_skin_1() {
         if (money >= 100f) {
-            skin_id = 1;
+            skin_1_buy = true;
             money -= 100f;
+        }
+    }
+
+    public void skin_1_set() {
+        if (skin_1_buy) {
+            skin_id = 1;
         }
     }
 }
