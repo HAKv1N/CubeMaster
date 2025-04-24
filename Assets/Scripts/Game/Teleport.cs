b12995e71dd8a571d02f07e9a3481c95b80debb2
HAKv1N <egor.pestushko@yandex.ru>
2025-04-24 11:24:16 +0300

    Update 31
    1. Добавлены телепорты
    2. Добавлены префабы телепортов
    
    by HAKv1N<3

---
+++ Assets/Scripts/Game/Teleport.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    [SerializeField] private GameObject teleport_in;
+    [SerializeField] private GameObject teleport_out;
+    [SerializeField] private Vector3 teleport_out_vector;
+    [SerializeField] private static bool can_teleport = true;
+    [SerializeField] private GameObject effects;
+    [SerializeField] private GameObject player;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (CompareTag("Teleport") && other.CompareTag("Player") && can_teleport) {
+            player = other.gameObject;
+            StartCoroutine("teleport_on");
+        }
+    }
+
+    IEnumerator teleport_on() {
+        effects.SetActive(true);
+        teleport_out_vector = teleport_out.transform.position;
+        can_teleport = false;
+        Player.can_move = false;
+        yield return new WaitForSeconds(1);
+        player.transform.position = teleport_out_vector;
+        Player.can_move = true;
+        yield return new WaitForSeconds(7);
+        effects.SetActive(false);
+        can_teleport = true;
+    }
+}
