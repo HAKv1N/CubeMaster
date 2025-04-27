ddfa5959d40fa51c806c695a71d96a53a939cc85
CountryOld2 <varkerktop@gmail.com>
2025-04-27 17:31:58 +0300

    Update15
    Сделана механика открытия двери ключом.

---
+++ Assets/Scenes/number = 2.py
@@ -0,0 +1,10 @@
+number = 2
+
+string = "Hello, World"
+
+result = True
+
+if number >= 2:
+    print("Excellent")
+else:
+    print("Bad")
--- Assets/Scripts/Game/KeyActivator.cs
+++ Assets/Scripts/Game/KeyActivator.cs
@@ -1,20 +1,29 @@
 using UnityEngine;
 using System.Collections;
+using UnityEngine.AI;
 
 public class KeyActivator : MonoBehaviour
 {
     [SerializeField] private GameObject effects;
-
+    private int key = 0;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("key"))
         {
             Destroy(other.gameObject);
-            if (other.CompareTag("Door"))
-            {
-                Destroy(other.gameObject);
-            }
+            key += 1;
         }
+    
+        
+    }
+    private void OnCollisionEnter(Collision other)
+    {
+    if(key == 1){ 
+        if(other.gameObject.tag == "Door")
+        {
+          Destroy(other.gameObject);
+        }
+       }
     }
 
 
